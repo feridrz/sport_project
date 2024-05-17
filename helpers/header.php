0752659140dc5c53f288413791bd6825ec72b1a9
Farid Rzayev <ferid_7003@mail.ru>
2024-05-17 23:04:51 +0400

    first

---
+++ helpers/header.php
@@ -0,0 +1,174 @@
+<header class="header">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col">
+        <nav class="navbar navbar-expand-lg">
+          <div class="navbar-nav">
+            <div class="nav-item dropdown position-static">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="studentsDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Tələbələr üçün
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="studentsDropdown">
+                <li>
+                  <a class="dropdown-item" href="/students1">Студенты 1</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="/students2">Студенты 2</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="/students3">Студенты 3</a>
+                </li>
+              </ul>
+            </div>
+            <div class="nav-item dropdown" style="margin-left: 29px">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="mediaDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Media Mərkəzi
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="mediaDropdown">
+                <li><a class="dropdown-item" href="/page1">Страница 1</a></li>
+                <li><a class="dropdown-item" href="/page2">Страница 2</a></li>
+                <li><a class="dropdown-item" href="/page3">Страница 3</a></li>
+              </ul>
+            </div>
+            <div class="d-flex" style="margin-left: 26px">
+              <a class="nav-link" href="#">Bloq</a>
+              <a class="nav-link" href="#" style="margin-left: 20px">Əlaqə</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div class="col-auto">
+        <img src="./public/search01.svg" alt="Search" class="img-fluid" />
+      </div>
+      <div class="col-auto">
+        <img src="./public/globe01.svg" alt="Globe" class="img-fluid" />
+      </div>
+      <div class="col-auto">
+        <a
+          class="nav-link dropdown-toggle"
+          href="#"
+          id="languageDropdown"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Dil menyusu
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="languageDropdown">
+          <li><a class="dropdown-item" href="/language1">Язык 1</a></li>
+          <li><a class="dropdown-item" href="/language2">Язык 2</a></li>
+          <li><a class="dropdown-item" href="/language3">Язык 3</a></li>
+        </ul>
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-outline-secondary" type="button">
+          test My ASAPES
+        </button>
+      </div>
+    </div>
+  </div>
+</header>
+
+<section class="section section-1">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-2">logo</div>
+      <div class="col-9 d-flex justify-content-end">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container-fluid">
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown1"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Haqqımızda
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                    <li><a class="dropdown-item" href="#">О нас</a></li>
+                    <li><a class="dropdown-item" href="#">Команда</a></li>
+                    <li><a class="dropdown-item" href="#">История</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown2"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Elm və tədris
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown2">
+                    <li><a class="dropdown-item" href="#">Факультеты</a></li>
+                    <li><a class="dropdown-item" href="#">Программы</a></li>
+                    <li><a class="dropdown-item" href="#">Расписание</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown3"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Tələbə həyatı
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown3">
+                    <li>
+                      <a class="dropdown-item" href="#">Жизнь на кампусе</a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">Клубы и организации</a>
+                    </li>
+                    <li><a class="dropdown-item" href="#">Общежитие</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Beynəlxalq əlaqələr</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Qəbul</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ index.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/global.css">
+</head>
+
+<body>
+  <?php include 'helpers/header.php'?>
+
+  <section class="agency">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5 p-0">
+          <div class="agency__left">
+            <div class="agency__title">
+              Azərbaycan Dövlət Bədən Tərbiyyəsi və İdman Akademiyası
+            </div>
+            <div class="agency__block">
+              <div class="agency__block-isolation d-flex justify-content-between">
+                <img src="assets/img/icons/Isolation_Mode.svg" alt="">
+                <button class="agency-btn">Azerbaycan</button>
+              </div>
+              <div class="agency__text">
+                <h2>Azərbaycan Milli Antidopinq Agentliyi ilə əməkdaşlıq haqqında memorandum imzalanıb</h2>
+              </div>
+              <div class="agency__about">
+                <a href="#" class="agency__about-a">
+                  Ətraflı oxu
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-7 p-0">
+          <div class="agency__slider">
+            <img src="assets\img\photo\slide-img-1.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="education">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-12">
+                  <div class="education__title">
+                      ASAPES-de tehsil
+                  </div>
+              </div>
+              <div class="col-md-3">
+                  <div class="education__block">
+                      <div class="education__block-title">
+                              <div class="education-block bg-light" style="height: 298px;">
+                              <div>Bakalavriat</div>
+                              <img src="assets/img/photo/e-1.png" alt="" class="mr-3">
+
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3">
+                  <div class="education__block">
+                      <div class="education__block-title">
+                              <div class="education-block bg-light" style="height: 298px;">
+                              <div>Magistratura</div>
+                              <img src="assets/img/photo/e-1.png" alt="" class="mr-3">
+
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3">
+                  <div class="education__block">
+                      <div class="education__block-title">
+                              <div class="education-block bg-light" style="height: 298px;">
+                              <div>Doktorantura</div>
+                              <img src="assets/img/photo/e-1.png" alt="" class="mr-3">
+
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3">
+                  <div class="education__block">
+                      <div class="education__block-title">
+                              <div class="education-block bg-light" style="height: 298px;">
+                              <div>Əlavə təhsil</div>
+                              <img src="assets/img/photo/e-1.png" alt="" class="mr-3">
+
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          </div>
+      </div>
+  </section>
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
